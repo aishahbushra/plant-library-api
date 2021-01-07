56aef79d3eca5e84639b9a781e9010c288e5592d
Aishah Bushra <aishahbushra@outlook.com>
2021-01-07 21:23:43 +0000

    CRUD operations created for plants

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+// app.get('/', (req, res) => {
+//     res.send("Hello World")
+// })
 
 app.post('/plant', plantController.create);
 
 app.get('/plant', plantController.list);
+
+app.patch('/plant/:id', plantController.updateById);
+
+app.delete('/plant/:plantId', plantController.deleteById);
 
 module.exports = app;
--- src/controllers/plant.js
+++ src/controllers/plant.js
@@ -7,3 +7,26 @@ exports.create = (req, res) => {
 exports.list = (req, res) => {
     Plant.findAll().then(plant => res.status(200).json(plant));
 };
+
+exports.updateById = (req, res) => {
+    const { id } = req.params;
+    console.log("update by id:", id);
+    Plant.update(req.body, { where: { id }}).then(([updatedPlant]) => {
+        if (!updatedPlant) {
+        res.status(404).json({ error: 'The plant could not be found.'});
+        } else {
+        res.status(200).json(updatedPlant);
+        }
+    });
+};
+
+exports.deleteById = (req, res) => {
+    const { plantId } = req.params;
+    Plant.destroy({ where: { plantId }}).then(([updatedPlant]) => {
+        if (!updatedPlant) {
+            res.status(404).json({ error: 'The plant could not be found.'});    
+        } else {
+            res.status(204).json(updatedPlant);
+        }
+    });
+};
