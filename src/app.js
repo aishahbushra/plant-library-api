dad81cfbd4a17b32af6ef45d9e386390c1ea33e4
Aishah Bushra <aishahbushra@outlook.com>
2021-03-09 20:40:34 +0000

    cors dependency added and enabled

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const plantController = require('./controllers/plant');
-
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("WELCOME TO THE PLANT API!")
